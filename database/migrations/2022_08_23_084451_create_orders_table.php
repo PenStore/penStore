d6f9f7ae0fe7a1f6d25240876573610ac34151a4
Richmond <rihards.gaislers@gmail.com>
2022-08-23 11:56:53 +0300

    Game added to 404 page

---
+++ database/migrations/2022_08_23_084451_create_orders_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->integer('product_id');
+            $table->integer('quantity');
+            $table->string('ShipAddress');
+            $table->string('ShipCity');
+            $table->string('ShipRegion');
+            $table->string('ShipZip');
+            $table->string('ShipCountry');
+            $table->date('OrderDate');
+            $table->float('weight');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+};

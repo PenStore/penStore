7e31d4e66ca47c9c91faad1d48e328ea6e6b0f62
De4d0ne <marciscevers@gmail.com>
2022-08-26 12:27:50 +0300

    fixed about us text in dropdown

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -12,7 +12,7 @@
                 <p>Black pens</p>
                 <p>Blue pens</p>
                 <p>Red pens</p>
-                <a>About us</p>
+                <p>About us</p>
                 <div class="bottom">
                     @if(!Auth::check())
                         <div class="login_box">
